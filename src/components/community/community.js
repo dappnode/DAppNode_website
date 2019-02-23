5f2ac572873db316773c4d35da229a6ae9568868
Eduardo Antuña Díez <20141918+eduadiez@users.noreply.github.com>
2019-02-23 13:58:37 +0100

    Update community.js

--- src/components/community/community.js
+++ src/components/community/community.js
@@ -22,28 +22,28 @@ const Community = () => {
     </ul>
     {/* TODO: Use gatsby-source-medium to retrieve this content instead of hard coding it in as it currently is.*/}
     <div className='blog'>
-      <a href='https://medium.com/@DAppNode/dappnode-loves-bufficorns-ethdenverimpact-5e6ddbe74fbc' target='_blank' rel="noopener noreferrer"><div>
+      <a href='https://medium.com/dappnode/dappnode-will-soon-sell-physical-dappnodes-d9a58a4ab892' target='_blank' rel="noopener noreferrer"><div>
         <div className='blog1'></div>
+        <div>
+          <h3>DAppNode will soon sell physical dappnodes!</h3>
+        </div>
+      </div></a>
+      <a href='https://medium.com/@DAppNode/dappnode-loves-bufficorns-ethdenverimpact-5e6ddbe74fbc' target='_blank' rel="noopener noreferrer"><div>
+        <div className='blog2'></div>
         <div>
           <h3>DAppNode loves #Bufficorns #ETHDenverImpact</h3>
         </div>
       </div></a>
       <a href='https://medium.com/@DAppNode/dappnode-and-constantinople-postponement-bdec66a62aba' target='_blank' rel="noopener noreferrer"><div>
-        <div className='blog2'></div>
+        <div className='blog3'></div>
         <div>
           <h3>DAppNode Constantinople Postponement</h3>
         </div>
       </div></a>
       <a href='https://medium.com/@DAppNode/dappnode-nfts-have-arrived-237e0c99712b' target='_blank' rel="noopener noreferrer"><div>
-        <div className='blog3'></div>
-        <div>
-          <h3>DAppNode NFT's Have Arrived!</h3>
-        </div>
-      </div></a>
-      <a href='https://medium.com/@DAppNode/dappnodes-gift-to-labitconf-6129d53b0f71' target='_blank' rel="noopener noreferrer"><div>
         <div className='blog4'></div>
         <div>
-          <h3>DAppNode's Git to LaBITConf</h3>
+          <h3>DAppNode NFT's Have Arrived!</h3>
         </div>
       </div></a>
       <a href='https://medium.com/@DAppNode/dappnode-the-infrastructure-for-the-decentralized-world-85983b16db14' target='_blank' rel="noopener noreferrer"><div>
