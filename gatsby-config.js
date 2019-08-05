16685cf8a12063d6f102165b8abb5884da9559a7
dapplion <guillem.lion.benosa@gmail.com>
2019-08-05 08:36:11 +0200

    Fix metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `DAppNode website`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
