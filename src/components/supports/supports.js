05ab2ee6554509d83e7c3dd41053bd290b0fa903
dapplion <guillem.lion.benosa@gmail.com>
2020-11-26 09:22:34 +0000

    Fix images and svgs

--- src/components/supports/supports.js
+++ src/components/supports/supports.js
@@ -3,80 +3,89 @@ import React from 'react'
 import './main.css'
 
 // Import png's
-import euro from '../../images/eu.png'
-import blockis from '../../images/blockis.png'
-import ngiTrust from '../../images/ngiTrust.png'
-
-// Import package to handle image hover-over color change.
-import HoverImage from 'react-hover-image'
+import europeanUnionLogo from '../../images/eu.png'
+import blockisLogo from '../../images/blockis.png'
+import ngiTrustLogo from '../../images/ngiTrust.png'
 
 // Import seperate images (non-hover color).
-import ef from '../../images/vectors/ef.svg'
-import aragon from '../../images/vectors/nest.svg'
-import giveth from '../../images/vectors/giveth.svg'
-import ecf from '../../images/vectors/ecf.svg'
+import efLogo from '../../images/vectors/ef.svg'
+import aragonLogo from '../../images/vectors/nest.svg'
+import givethLogo from '../../images/vectors/giveth.svg'
+import ecfLogo from '../../images/vectors/ecf.svg'
 
-// Import seperate images (hover color).
-import efhover from '../../images/vectors/efhover.svg'
-import givethhover from '../../images/vectors/givethhover.svg'
-import aragonhover from '../../images/vectors/nesthover.svg'
-import ecfhover from '../../images/vectors/ecfhover.svg'
+const newTabProps = { target: '_blank', rel: 'noopener noreferrer' }
+const supportersSvgLogos = [
+  {
+    alt: 'Ethereum Foundation',
+    src: efLogo,
+    href:
+      'https://blog.ethereum.org/2018/08/17/ethereum-foundation-grants-update-wave-3/',
+  },
+  {
+    alt: 'Ethereum Community Fund',
+    src: ecfLogo,
+    href: 'https://ecf.network/',
+  },
+  {
+    alt: 'Aragon Nest',
+    src: aragonLogo,
+    href:
+      'https://blog.aragon.org/aragon-nest-second-round-of-grants/#dappnode',
+  },
+  {
+    alt: 'Giveth',
+    src: givethLogo,
+    href: 'https://beta.giveth.io/campaigns/5b44b198647f33526e67c262',
+  },
+]
+const supportersPng = [
+  {
+    alt: 'European Union',
+    src: europeanUnionLogo,
+    href: 'https://ec.europa.eu/',
+  },
+  {
+    alt: 'Block IS',
+    src: blockisLogo,
+    href: 'https://blockis.eu/',
+  },
+  {
+    alt: 'NGI trust',
+    src: ngiTrustLogo,
+    href: 'https://www.ngi.eu/ngi-projects/ngi-trust/',
+  },
+]
 
 // Declare supports section.
 const Supports = () => (
   <div className="section supports">
-    <p>Supported through grants and funds from: </p>
-    {/* Seperate grants are linked accordingly */}
-    <a
-      href="https://blog.ethereum.org/2018/08/17/ethereum-foundation-grants-update-wave-3/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <HoverImage src={ef} hoverSrc={efhover} alt="Ethereum Foundation" />
-    </a>
-    <a href="https://ecf.network/" target="_blank" rel="noopener noreferrer">
-      <HoverImage src={ecf} hoverSrc={ecfhover} alt="Ethereum Community Fund" />
-    </a>
-    <a
-      href="https://blog.aragon.org/aragon-nest-second-round-of-grants/#dappnode"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <HoverImage src={aragon} hoverSrc={aragonhover} alt="Aragon Nest" />
-    </a>
-    <a
-      href="https://beta.giveth.io/campaigns/5b44b198647f33526e67c262"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <HoverImage src={giveth} hoverSrc={givethhover} alt="Giveth" />
-    </a>
+    <div className="p-wrapper">
+      <p>Supported through grants and funds from: </p>
+    </div>
+
+    {supportersSvgLogos.map(({ alt, src, href }) => (
+      <a href={href} {...newTabProps}>
+        <img src={src} alt={alt} className="svg-logo" />
+      </a>
+    ))}
 
     <br />
 
-    <div>
-      <img src={euro} />
-      <a href="https://blockis.eu/" target="_blank" rel="noopener noreferrer">
-        <HoverImage src={blockis} hoverSrc={blockis} alt="Block IS" />
+    {supportersPng.map(({ alt, src, href }) => (
+      <a href={href} {...newTabProps}>
+        <img src={src} alt={alt} />
       </a>
-      <a
-        href="https://www.ngi.eu/ngi-projects/ngi-trust/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <HoverImage src={ngiTrust} hoverSrc={ngiTrust} alt="NGI trust" />
-      </a>
-    </div>
+    ))}
 
-    <a>
-      <p>
+    <div className="p-wrapper">
+      <p className="european-union-bottom-text">
         This project has received funding from the European Union’s Horizon 2020
         research and innovation programme under the NGI_TRUST grant agreement no
         825618, and indirectly received funding from the European Union’s
         Horizon 2020 research and innovation programme under project Block.IS
         (grant agreement No 824509).
       </p>
-    </a>
+    </div>
   </div>
 )
 
