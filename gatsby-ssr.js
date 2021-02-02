608b5f88bdbe528c3ce59dea2598018966ad53ed
pablo <mendez4a@gmail.com>
2021-02-02 16:50:04 +0100

    added script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,18 @@
 const React = require('react')
 
+setHeadComponents([
+  <script
+    async
+    defer
+    data-domain="dappnode.io"
+    src="https://plausible.io/js/plausible.js"
+  ></script>,
+])
+
 exports.onRenderBody = ({ setPreBodyComponents }) => {
   setPreBodyComponents([
-    <noscript key="noscript">Your browser does not support JavaScript!</noscript>,
+    <noscript key="noscript">
+      Your browser does not support JavaScript!
+    </noscript>,
   ])
 }
