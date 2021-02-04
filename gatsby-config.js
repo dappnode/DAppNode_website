fd08b59641982a55cf6040276dac1e2b021fd56f
pablo <mendez4a@gmail.com>
2021-02-04 16:00:42 +0100

    fix versions dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-plausible',
+      options: {
+        domain: 'dappnode.io',
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`, //fix this
     {
